0da29325b8f7eaf540e8903374eff586753eb4a2
kachow4 <kchow1sfo@gmail.com>
2023-07-30 00:13:14 -0700

    fix text sizing from md to base

--- components/core/Contact.tsx
+++ components/core/Contact.tsx
@@ -9,9 +9,9 @@ export default function Contact() {
   
     <div className="flex bg-dark-green px-8 py-5 tablet:px-24 tablet:py-10 justify-center">
       <div id="contact" className='flex flex-col gap-2 tablet:gap-5 bg-dark-green text-white items-center tablet:p-12'>
-            <span className='font-bold text-md tablet:text-2xl'>Contact Me</span>
+            <span className='font-bold text-base tablet:text-2xl'>Contact Me</span>
 
-            <p className='text-sm tablet:text-md'>Please feel free to reach out! I would love connect and discuss open opportunities and receive any feedback 
+            <p className='text-sm tablet:text-base'>Please feel free to reach out! I would love connect and discuss open opportunities and receive any feedback 
                 on my work.</p>
             
             <ul className="flex p-2 gap-10 w-full justify-center">
